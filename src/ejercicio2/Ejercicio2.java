c52c7ea7e3a79bea858064ba79fb0a0189995ea5
camilagarcia36 <garciiaperezz@gmail.com>
2023-03-07 12:50:27 +0100

    ejercicio completo

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,25 @@
+package ejercicio2;
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+
+	//Ejercicio 2 
+	
+	System.out.println("\n \nEJERCICIO 2 ");
+	
+	int N = 10; 
+	double A = 4.567 ; 
+	char C= 'd' ; 
+	int valorNumericoC = Character.getNumericValue(C); 
+	
+	
+	
+	System.out.println("Valor de N: " + N +  ";\nValor de A: " + A + ";\nValor de C: " + C);
+
+	System.out.println("Suma N+A: " + (N+A));
+	System.out.println("Diferencia N-A: " + (N-A));
+	
+	System.out.println("Valor numerico del Char C: " + valorNumericoC);
+
+}
+	}
